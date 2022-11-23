6a8e00454a549f64f0c646e13fdc73d0e0d62707
orasanen <okko.rasanen@tuni.fi>
2022-11-23 10:18:18 +0200

    output path

--- CDI_lextest.sh
+++ CDI_lextest.sh
@@ -7,11 +7,11 @@ if [ "$#" -lt 2 ]; then
 fi
 
 if [ "$#" -eq 2 ]; then
-  matlab -batch "CDI_lextest '$1' '$2';";
+  matlab -batch "CDI_lextest '$1' '$2';" > /dev/null;
 elif [ "$#" -eq 3 ]; then
-  matlab -batch "CDI_lextest '$1' '$2' '$3';";
+  matlab -batch "CDI_lextest '$1' '$2' '$3';" > /dev/null;
 elif [ "$#" -eq 4 ]; then
-  matlab -batch "CDI_lextest '$1' '$2' '$3' $4;";
+  matlab -batch "CDI_lextest '$1' '$2' '$3' $4;" > /dev/null;
 elif [ "$#" -eq 5 ]; then
   matlab -batch "CDI_lextest '$1' '$2' '$3' $4 '$5';" > /dev/null;
 fi
